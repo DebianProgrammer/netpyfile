8ef2ed71607331b5147d1810624bc0c5a7396bc3
DebianProgrammer <34977208+DebianProgrammer@users.noreply.github.com>
2021-08-15 18:04:55 -0700

    view file sizes in list

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import hashlib
 import ast
 from getpass import getpass
 
-connip = "192.168.0.102"
+connip = "192.168.0.101"
 connport = 4000
 
 c = socket.socket()
@@ -16,66 +16,66 @@ isauth = c.recv(1024).decode()
 print(welcome.decode())
 
 if(isauth == "auth is active"):
-	print(isauth)
-	sendpass = getpass("Enter password (will not be echoed): ")
-	passcheck = hashlib.md5(sendpass.encode("utf-8")).hexdigest()
-	c.send(passcheck.encode())
-	check = c.recv(1024).decode()
-	if(check == "correct"):
-		print("correct password")
-	else:
-		print("incorrect password")
-		quit()
+    print(isauth)
+    sendpass = getpass("Enter password (will not be echoed): ")
+    passcheck = hashlib.md5(sendpass.encode("utf-8")).hexdigest()
+    c.send(passcheck.encode())
+    check = c.recv(1024).decode()
+    if(check == "correct"):
+        print("correct password")
+    else:
+        print("incorrect password")
+        quit()
 
 
 while True:
-	sender = input("fileshell> ")
-	if(sender == "download"):
-		choose = input("Name the file you want to download: ")
-		newname = input("What name do you want to save it as? ")
-		#check if file exists
-		c.send("query".encode())
-		c.send(choose.encode())
-		check = c.recv(1024)
-		check = check.decode()
-		if(check == "Does not"):
-			print("The file does not exist")
-			continue
-		c.send(sender.encode())
-		c.send(choose.encode())
-		print("File exists! Downloading file...")
-		newfile = open(newname, "wb")
-		content = c.recv(1024)
-		while(content):
-			try:
-				newfile.write(content)
-				content = c.recv(1024)
-			except socket.timeout:
-				newfile.close()
-				break
-				continue
-			except ValueError:
-				break
-				continue
-		newfile.close()
-		print("Done downloading")
-		continue
-	if(sender == "list"):
-		sender = sender.encode()
-		c.send(sender)
-		warnnote = c.recv(1024)
-		print(warnnote.decode())
-		print(" ")
-		filelist = c.recv(4096)
-		filelist = filelist.decode()
-		filelist = ast.literal_eval(filelist)
-		for file in filelist:
-			print(file)
-		continue
-	sender = sender.encode()
-	c.send(sender)
-	textback = c.recv(1024)
-	textback = textback.decode()
-	print(textback)
-	
-	
+    sender = input("fileshell> ")
+    if(sender == "download"):
+        choose = input("Name the file you want to download: ")
+        newname = input("What name do you want to save it as? ")
+        #check if file exists
+        c.send("query".encode())
+        c.send(choose.encode())
+        check = c.recv(1024)
+        check = check.decode()
+        if(check == "Does not"):
+            print("The file does not exist")
+            continue
+        c.send(sender.encode())
+        c.send(choose.encode())
+        print("File exists! Downloading file...")
+        newfile = open(newname, "wb")
+        content = c.recv(1024)
+        while(content):
+            try:
+                newfile.write(content)
+                content = c.recv(1024)
+            except socket.timeout:
+                newfile.close()
+                break
+                continue
+            except ValueError:
+                break
+                continue
+        newfile.close()
+        print("Done downloading")
+        continue
+    if(sender == "list"):
+        sender = sender.encode()
+        c.send(sender)
+        warnnote = c.recv(1024)
+        print(warnnote.decode())
+        print(" ")
+        filelist = c.recv(4096)
+        filelist = filelist.decode()
+        filelist = ast.literal_eval(filelist)
+        for file in filelist:
+            print(file)
+        continue
+    sender = sender.encode()
+    c.send(sender)
+    textback = c.recv(1024)
+    textback = textback.decode()
+    print(textback)
+    
+    
--- server.py
+++ server.py
@@ -22,53 +22,70 @@ print(addr)
 wel = f"Welcome to {socket.gethostname()}"
 sock.send(wel.encode())
 
+def readsize(size):
+    for u in ["B", "KiB", "MiB", "GiB", "TiB"]:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+        return f"{size:.3f}{u}"
+
 if(authenabled == 1):
-	sock.send("auth is active".encode())
-	passhash = sock.recv(1024).decode()
-	if(passhash == authhash):
-		sock.send("correct".encode())
-	else:
-		sock.send("incorrect".encode())
-		s.close()
-		quit()
+    sock.send("auth is active".encode())
+    passhash = sock.recv(1024).decode()
+    if(passhash == authhash):
+        sock.send("correct".encode())
+    else:
+        sock.send("incorrect".encode())
+        s.close()
+        quit()
 else:
-	sock.send("no auth is needed".encode())
+    sock.send("no auth is needed".encode())
 
 while True:
-	filecommand = sock.recv(1024)
-	if(filecommand.decode() == "help"):
-		back = "commands:\nhelp - list this\nlist - list directory\ndownload - lets you download a file\ncloseserver - close socket"
-		sock.send(back.encode())
-	elif(filecommand.decode() == "list"):
-		note = "Note: this only lists files, dirs are not supported"
-		sock.send(note.encode())
-		filelist = [f for f in os.listdir(serverpath) if os.path.isfile(os.path.join(serverpath,f))]
-		filelist = str(filelist)
-		sock.send(filelist.encode())
-	elif(filecommand.decode() == "download"):
-		whatfile = sock.recv(1024).decode()
-		if(os.path.exists(serverpath + "/" + whatfile)):
-			#goodnote = "exists"
-			#goodcheck = sock.send(goodnote.encode())
-			openfile = serverpath + "/" + whatfile
-			sendfile = open(openfile, "rb")
-			readcontent = sendfile.read(1024)
-			while(readcontent):
-				sock.send(readcontent)
-				readcontent = sendfile.read(1024)
-			sendfile.close()
-		else:
-			cool = "no file"
-			sock.send(cool.encode())
-	elif(filecommand.decode() == "query"):
-		checkfile = sock.recv(1024).decode()
-		if(os.path.exists(serverpath + "/" + checkfile)):
-			sock.send("Does".encode())
-		else:
-			sock.send("Does not".encode())	
-	elif(filecommand.decode() == "closeserver"):
-		s.close()
-		quit()
-	else:
-		back = "invalid command - use help for help"
-		sock.send(back.encode())
+    filecommand = sock.recv(1024)
+    if(filecommand.decode() == "help"):
+        back = "commands:\nhelp - list this\nlist - list directory\ndownload - lets you download a file\ncloseserver - close socket"
+        sock.send(back.encode())
+    elif(filecommand.decode() == "list"):
+        note = "Note: this only lists files, dirs are not supported"
+        sock.send(note.encode())
+        size = []
+        filelist = [f for f in os.listdir(serverpath) if os.path.isfile(os.path.join(serverpath,f))]
+        #filelist = str(filelist)
+        #sock.send(filelist.encode())
+        for i in os.listdir(serverpath):
+            si = os.path.getsize(serverpath + "/" + i)
+            si = readsize(si)
+            size.append(si)
+        filelist2 = []
+        for x, y in zip(filelist, size):
+            filelist2.append(f"{x} -- {y}")
+        filelist2 = str(filelist2)
+        sock.send(filelist2.encode())
+    elif(filecommand.decode() == "download"):
+        whatfile = sock.recv(1024).decode()
+        if(os.path.exists(serverpath + "/" + whatfile)):
+            #goodnote = "exists"
+            #goodcheck = sock.send(goodnote.encode())
+            openfile = serverpath + "/" + whatfile
+            sendfile = open(openfile, "rb")
+            readcontent = sendfile.read(1024)
+            while(readcontent):
+                sock.send(readcontent)
+                readcontent = sendfile.read(1024)
+            sendfile.close()
+        else:
+            cool = "no file"
+            sock.send(cool.encode())
+    elif(filecommand.decode() == "query"):
+        checkfile = sock.recv(1024).decode()
+        if(os.path.exists(serverpath + "/" + checkfile)):
+            sock.send("Does".encode())
+        else:
+            sock.send("Does not".encode())  
+    elif(filecommand.decode() == "closeserver"):
+        s.close()
+        quit()
+    else:
+        back = "invalid command - use help for help"
+        sock.send(back.encode())
